4ac6855145a98357c17ab57a5218a717d3775d37
Lance PJC <lancePJC@github.com>
2025-01-16 18:05:46 +0300

    updates

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  output: 'standalone', // For SSR and dynamic content
+  output: 'export', // For SSR and dynamic content
     images: {
       domains: ['utfs.io','cdn.shopify.com', 'www.phoneplacekenya.com', 'q896co9uas.ufs.sh'], // Add both domains
     },
